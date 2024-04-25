d317c5c4698b6b1861297acfa97f871e681e775f
DmitryTeplov182 <DmitryTeplov182@users.noreply.github.com>
2024-04-25 18:20:33 +0400

    Add ServerStop and ServerStart and Examples
    (cherry picked from commit 94332883f1efe2ba484e99fcaea5fef3dd6dbfae)
    (cherry picked from commit 6307fb38ed205c5a95e93f21553b7c2f58925737)
    (cherry picked from commit 037a20635b8dcf1bebd89c27d17341d302b02f8b)

---
+++ _examples/server-start.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/nathanielvarona/pritunl-api-go"
+)
+
+func main() {
+	// Initialize the Pritunl API client
+	client, err := pritunl.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Retrieve the server ID from an environment variable
+	server := os.Getenv("PRITUNL_DATA_SERVER")
+
+	// Create a context for the request
+	ctx := context.Background()
+
+	// Delete the specified server
+	_, err = client.ServerStart(ctx, server)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print a success message
+	fmt.Println("Server Started Successfully")
+}
